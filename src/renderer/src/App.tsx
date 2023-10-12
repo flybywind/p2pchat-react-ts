d8fb4e08e2ec61f516ccfbf5c686d39ccefd6499
tongxin.wen <flybywind@foxmail.com>
2023-10-12 17:30:38 +0800

    add search and filter header

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
   const [screen, setScreen] = useState('square')
 
   function isActive(s: string): string {
-    return screen === s ? 'col s12 show' : 'hide'
+    return screen === s ? '' : 'is-hidden'
   }
 
   function activeScreen(s: string) {
@@ -16,36 +16,63 @@ function App(): JSX.Element {
 
   return (
     <>
-      <div className="container row">
-        <div id="square" className={'screen ' + isActive('square')}>
-          <h3>Topic Square</h3>
-          <div className="divider"></div>
-          <ul className="collection">
-            <li className="collection-item first-item">
-              <span>Topic</span>
-              <span className="secondary-content">Description</span>
-            </li>
-            <TopicCard topicId={'123'} topicDesc="Hello world" />
-            <TopicCard topicId={'3-4-5'} topicDesc="Hello world" />
-          </ul>
-        </div>
-        <div id="chatroom" className={'screen ' + isActive('chatroom')}>
-          <div className="row">
-            <div className="topicside col s4">
-              <ul>
-                <li>topic1</li>
-                <li>topic1</li>
-                <li>topic1</li>
-                <li>topic1</li>
-              </ul>
+      <div id="square" className={'screen box ' + isActive('square')}>
+        <nav className="navbar is-link" role="navigation" aria-label="dropdown navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/square">
+              Square
+            </a>
+          </div>
+          <div className="navbar-start">
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">Location</a>
+              <div className="navbar-dropdown">
+                <div className="navbar-item">&lt; 10km</div>
+                <div className="navbar-item">&lt; 100km</div>
+                <div className="navbar-item">&lt; 1000km</div>
+                <div className="navbar-item">global</div>
+              </div>
             </div>
-            <div className="chat-window col s8">
-              <div className="chatsection">
-                <p>Hello</p>
-              </div>
-              <div className="editmessage">
-                <textarea name="messagebox" id="messagebox" cols={30} rows={2}></textarea>
-              </div>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <input type="text" className="input" />
+              <button className="button">
+                <span className="icon is-small">
+                  <span className="fas fa-search" aria-hidden="true"></span>
+                </span>
+                <span>Search</span>
+              </button>
+            </div>
+          </div>
+        </nav>
+        <div className="box mt-4 ml-5">
+          <TopicCard topicId={'123'} topicName={'chat anything'} topicDesc="Hello world" />
+          <TopicCard topicId={'3-4-5'} topicName={'chat with lily'} topicDesc="Hello world" />
+          <TopicCard topicId={'123'} topicName={'chat anything'} topicDesc="Hello world" />
+          <TopicCard topicId={'123'} topicName={'chat anything'} topicDesc="Hello world" />
+          <TopicCard topicId={'123'} topicName={'chat anything'} topicDesc="Hello world" />
+          <TopicCard topicId={'3-4-5'} topicName={'chat with lily'} topicDesc="Hello world" />
+          <TopicCard topicId={'3-4-5'} topicName={'chat with lily'} topicDesc="Hello world" />
+          <TopicCard topicId={'3-4-5'} topicName={'chat with lily'} topicDesc="Hello world" />
+        </div>
+      </div>
+      <div id="chatroom" className={'screen box ' + isActive('chatroom')}>
+        <div className="row">
+          <div className="topicside col s4">
+            <ul>
+              <li>topic1</li>
+              <li>topic1</li>
+              <li>topic1</li>
+              <li>topic1</li>
+            </ul>
+          </div>
+          <div className="chat-window col s8">
+            <div className="chatsection">
+              <p>Hello</p>
+            </div>
+            <div className="editmessage">
+              <textarea name="messagebox" id="messagebox" cols={30} rows={2}></textarea>
             </div>
           </div>
         </div>
--- src/renderer/src/components/TopicCard.tsx
+++ src/renderer/src/components/TopicCard.tsx
@@ -1,15 +1,24 @@
 interface Props {
   topicId: string
+  topicName: string
   topicDesc: string
 }
-export default function TopicCard({ topicId, topicDesc }: Props): JSX.Element {
+export default function TopicCard({ topicId, topicName, topicDesc }: Props): JSX.Element {
   return (
-    <li className="topic-card collection-item ">
-      <a>
-        <span className="material-icons">tag_faces</span>
-        <span>{topicId}</span>
-      </a>
-      <span className="secondary-content"> {topicDesc} </span>
-    </li>
+    <div className="card mb-4">
+      <div className="media ml-4">
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{topicName}</strong>&space;@{topicId}
+              <span className="icon is-small ml-4">
+                <span className="fas fa-arrow-circle-right" aria-hidden="true"></span>
+              </span>
+            </p>
+          </div>
+          <div className="content"> {topicDesc} </div>
+        </div>
+      </div>
+    </div>
   )
 }
